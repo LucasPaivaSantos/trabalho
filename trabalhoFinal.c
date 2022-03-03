5ef1f3b0cc6b6c701248a7f4cf0f2b376179a44d
LucasPaivaSantos <lucaspaiva@live.com>
2022-03-03 18:19:54 -0300

    versão final que será apresentada dia 3 de março 19h

--- trabalhoFinal.c
+++ trabalhoFinal.c
@@ -5,7 +5,7 @@
 #define COLUNAS 24
 #define NUMTREINADORES 4
 #define NUMCIDADES 16
-#define TAMALGODEX 15
+#define TAMALGODEX 18
 #define TAMMENU 3
 
 typedef struct
@@ -49,12 +49,6 @@ typedef struct
     Cidade cidade;
     char mapa[LINHAS][COLUNAS];
 } ResultadoBatalha;
-
-typedef struct
-{
-    Jogador jogador;
-    int indicePrimeiroExibido;
-} ResultadoAcao;
 
 Algomon criaAlgomon(char nome[], int atk, int vida, char tipo)
 {
@@ -109,7 +103,7 @@ int possuiTodasAsInsignas(Jogador jogador)
 {
     if (jogador.insignias == NUMTREINADORES)
     {
-        for (int i = 0; i < 20; i++)
+        for (int i = 0; i < 21; i++)
         {
             printf("\n");
         }
@@ -127,7 +121,7 @@ int possuiTodasAsInsignas(Jogador jogador)
 
 void fimDeJogo()
 {
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 21; i++)
     {
         printf("\n");
     }
@@ -590,15 +584,13 @@ Jogador moverParaOLeste(char mapa[LINHAS][COLUNAS], Cidade cidadesExistentes[], 
     return jogador;
 }
 
-ResultadoAcao leAcao(char mapa[LINHAS][COLUNAS], Cidade cidadesExistentes[], Jogador jogador, int indicePrimeiroExibido)
+Jogador leAcao(char mapa[LINHAS][COLUNAS], Cidade cidadesExistentes[], Jogador jogador)
 {
-    ResultadoAcao resultado;
-    resultado.indicePrimeiroExibido = indicePrimeiroExibido;
 
     int y, x = 0;
 
     char movimento;
-    printf("Qual seu movimento (wasdrf)?");
+    printf("\nQual seu movimento (wasdrf)?");
     scanf("%c", &movimento);
     {
         switch (movimento)
@@ -632,8 +624,7 @@ ResultadoAcao leAcao(char mapa[LINHAS][COLUNAS], Cidade cidadesExistentes[], Jog
         }
     };
     scanf("%*c");
-    resultado.jogador = jogador;
-    return resultado;
+    return jogador;
 }
 
 void exibeMenu(Jogador jogador)
@@ -654,41 +645,45 @@ int main()
     algomonsDisponiveis[0] = criaAlgomon("Ifssauro", 5, 20, 'C');
     algomonsDisponiveis[1] = criaAlgomon("Whiledle", 3, 40, 'R');
     algomonsDisponiveis[2] = criaAlgomon("Vectoray", 4, 30, 'D');
-    algomonsDisponiveis[3] = criaAlgomon("Elsenite", 5, 18, 'C');
-    algomonsDisponiveis[4] = criaAlgomon("Forlax", 3, 36, 'R');
-    algomonsDisponiveis[5] = criaAlgomon("Arrayzard", 2, 50, 'D');
-    algomonsDisponiveis[6] = criaAlgomon("Switcheve", 5, 22, 'C');
-    algomonsDisponiveis[7] = criaAlgomon("Doiling", 3, 40, 'R');
-    algomonsDisponiveis[8] = criaAlgomon("Stringle", 4, 32, 'D');
-    algomonsDisponiveis[9] = criaAlgomon("Elsifee", 7, 15, 'C');
-    algomonsDisponiveis[10] = criaAlgomon("Loopoise", 4, 25, 'R');
-    algomonsDisponiveis[11] = criaAlgomon("Structurer", 4, 50, 'D');
-    algomonsDisponiveis[12] = criaAlgomon("Ruby", 6, 42, 'C');
-    algomonsDisponiveis[13] = criaAlgomon("Javaprie", 5, 30, 'R');
-    algomonsDisponiveis[14] = criaAlgomon("Ceepluplus", 8, 50, 'L');
+    algomonsDisponiveis[3] = criaAlgomon("Elsenite", 4, 38, 'C');
+    algomonsDisponiveis[4] = criaAlgomon("Forlax", 4, 37, 'R');
+    algomonsDisponiveis[5] = criaAlgomon("Arrayzard", 5, 32, 'D');
+    algomonsDisponiveis[6] = criaAlgomon("Switcheve", 4, 55, 'C');
+    algomonsDisponiveis[7] = criaAlgomon("Doiling", 7, 23, 'R');
+    algomonsDisponiveis[8] = criaAlgomon("Stringle", 4, 43, 'D');
+    algomonsDisponiveis[9] = criaAlgomon("Elsifee", 7, 21, 'C');
+    algomonsDisponiveis[10] = criaAlgomon("Loopoise", 4, 41, 'R');
+    algomonsDisponiveis[11] = criaAlgomon("Structurer", 5, 29, 'D');
+    algomonsDisponiveis[12] = criaAlgomon("Endifee", 4, 39, 'C');
+    algomonsDisponiveis[13] = criaAlgomon("Jynext", 3, 55, 'R');
+    algomonsDisponiveis[14] = criaAlgomon("Mewboolean", 8, 23, 'D');
+    algomonsDisponiveis[15] = criaAlgomon("Unlesonix", 3, 47, 'C');
+    algomonsDisponiveis[16] = criaAlgomon("Foreach", 7, 22, 'R');
+    algomonsDisponiveis[17] = criaAlgomon("Eveeinteger", 5, 28, 'D');
+    algomonsDisponiveis[18] = criaAlgomon("Ceepluplus", 7, 40, 'L');
 
     Treinador treinadoresExistentes[NUMTREINADORES];
-    treinadoresExistentes[0] = criaTreinador('Z', algomonsDisponiveis[14]);
-    treinadoresExistentes[1] = criaTreinador('X', algomonsDisponiveis[13]);
-    treinadoresExistentes[2] = criaTreinador('Y', algomonsDisponiveis[12]);
+    treinadoresExistentes[0] = criaTreinador('Z', algomonsDisponiveis[18]);
+    treinadoresExistentes[1] = criaTreinador('X', algomonsDisponiveis[4]);
+    treinadoresExistentes[2] = criaTreinador('Y', algomonsDisponiveis[15]);
     treinadoresExistentes[3] = criaTreinador('R', algomonsDisponiveis[11]);
 
     Cidade cidadesExistentes[NUMCIDADES];
     cidadesExistentes[0] = criaCidade(6, 0, algomonsDisponiveis[3]);
-    cidadesExistentes[1] = criaCidade(12, 1, algomonsDisponiveis[4]);
-    cidadesExistentes[2] = criaCidade(18, 1, algomonsDisponiveis[5]);
-    cidadesExistentes[3] = criaCidade(0, 3, algomonsDisponiveis[6]);
-    cidadesExistentes[4] = criaCidade(6, 3, algomonsDisponiveis[7]);
-    cidadesExistentes[5] = criaCidade(12, 3, algomonsDisponiveis[8]);
-    cidadesExistentes[6] = criaCidade(18, 3, algomonsDisponiveis[9]);
-    cidadesExistentes[7] = criaCidade(10, 5, algomonsDisponiveis[10]);
-    cidadesExistentes[8] = criaCidade(22, 5, algomonsDisponiveis[11]);
-    cidadesExistentes[9] = criaCidade(6, 7, algomonsDisponiveis[12]);
-    cidadesExistentes[10] = criaCidade(10, 7, algomonsDisponiveis[13]);
-    cidadesExistentes[11] = criaCidade(16, 7, algomonsDisponiveis[14]);
-    cidadesExistentes[12] = criaCidadeComTreinador(0, 0, algomonsDisponiveis[14], treinadoresExistentes[0]);
-    cidadesExistentes[13] = criaCidadeComTreinador(22, 3, algomonsDisponiveis[13], treinadoresExistentes[1]);
-    cidadesExistentes[14] = criaCidadeComTreinador(0, 7, algomonsDisponiveis[12], treinadoresExistentes[2]);
+    cidadesExistentes[1] = criaCidade(12, 1, algomonsDisponiveis[5]);
+    cidadesExistentes[2] = criaCidade(18, 1, algomonsDisponiveis[6]);
+    cidadesExistentes[3] = criaCidade(0, 3, algomonsDisponiveis[7]);
+    cidadesExistentes[4] = criaCidade(6, 3, algomonsDisponiveis[8]);
+    cidadesExistentes[5] = criaCidade(12, 3, algomonsDisponiveis[9]);
+    cidadesExistentes[6] = criaCidade(18, 3, algomonsDisponiveis[10]);
+    cidadesExistentes[7] = criaCidade(10, 5, algomonsDisponiveis[12]);
+    cidadesExistentes[8] = criaCidade(22, 5, algomonsDisponiveis[13]);
+    cidadesExistentes[9] = criaCidade(6, 7, algomonsDisponiveis[14]);
+    cidadesExistentes[10] = criaCidade(10, 7, algomonsDisponiveis[16]);
+    cidadesExistentes[11] = criaCidade(16, 7, algomonsDisponiveis[17]);
+    cidadesExistentes[12] = criaCidadeComTreinador(0, 0, algomonsDisponiveis[18], treinadoresExistentes[0]);
+    cidadesExistentes[13] = criaCidadeComTreinador(22, 3, algomonsDisponiveis[4], treinadoresExistentes[1]);
+    cidadesExistentes[14] = criaCidadeComTreinador(0, 7, algomonsDisponiveis[15], treinadoresExistentes[2]);
     cidadesExistentes[15] = criaCidadeComTreinador(22, 7, algomonsDisponiveis[11], treinadoresExistentes[3]);
 
     Jogador jogador1;
@@ -716,9 +711,7 @@ int main()
         printf("\n");
         exibeMapa(mapa, jogador1);
         exibeMenu(jogador1);
-        ResultadoAcao resultado = leAcao(mapa, cidadesExistentes, jogador1, indicePrimeiroExibido);
-        jogador1 = resultado.jogador;
-        indicePrimeiroExibido = resultado.indicePrimeiroExibido;
+        jogador1 = leAcao(mapa, cidadesExistentes, jogador1);
     }
 
     return 0;
